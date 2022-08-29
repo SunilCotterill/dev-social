0a14899f2fb78b78575a515f51ad4b95de15cdfc
SunilCotterill <sunilcotterill@gmail.com>
2022-08-29 07:53:07 -0400

    finished posts and created react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+
+const App = () => (
+    <Fragment>
+      <Navbar />
+      <Landing />
+      <h1>App</h1>
+    </Fragment>
+);
+
+
+export default App;
