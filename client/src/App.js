0537316b39a339e48562fef78442e767a5fb55ca
SunilCotterill <sunilcotterill@gmail.com>
2022-08-30 00:05:54 -0400

    started on react register

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 
 const App = () => (
+  <Router>
     <Fragment>
       <Navbar />
-      <Landing />
-      <h1>App</h1>
+      <Routes>
+        <Route path='/' element={<Landing />} exact />
+      </Routes>
+      
+        <section className="container">
+        <Routes>
+          <Route path='/register' element={<Register />} exact />
+          <Route path='/Login' element={<Login />} exact />
+          </Routes>
+        </section>
+      
+
     </Fragment>
+  </Router>
 );
 
 
